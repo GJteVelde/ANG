66a070a1c94875cfe2a3fc81cc5d2c7946752572
GJ <gerjan.te.velde@gmail.com>
2019-01-11 22:18:25 +0100

    Initial Commit

---
+++ ANG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ANG
+//
+//  Created by Gerjan te Velde on 11/01/2019.
+//  Copyright © 2019 Gerjan te Velde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
